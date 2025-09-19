fc987640a1283105ddd900c00f4334eb75c8da00
SirJaey <smartjoshua98@gmail.com>
2025-09-19 15:49:07 +0100

    Convert project from JS to TypeScript

--- src/pages/CrashTest.jsx
+++
@@ -1,3 +0,0 @@
-export default function CrashTest() {
-	throw new Error("Check your link for errors or try relaoding this page");
-}
---
+++ src/pages/SingleTodo.tsx
@@ -0,0 +1,127 @@
+// import ErrorBoundary from "../components/ErrorBoundary";
+// import LoadingSpinner from "../components/LoadingSpinner";
+// import { Link } from "react-router-dom";
+
+// export default function SingleTodo({ details, error, loading, setDetails }) {
+// 	const handleClosePreview = () => {
+// 		setDetails(false); // Clear the preview
+// 	};
+
+// 	return (
+// 		<ErrorBoundary>
+// 			<div className="todo-item">
+// 				{loading && <LoadingSpinner />}
+// 				{error && <p className="text-red-600">Error: {error}</p>}
+// 				<div className="space-y-2 mb-4">
+// 					{details && !loading &&  (
+// 						<div>
+// 							<div className="preview modal-overlay">
+// 								<div key={details.id} className="modal-content">
+// 									<div>
+// 										<strong>Todo Details</strong>
+// 									</div>
+// 									<div>
+// 										<strong>ID:</strong> {details.id || "Your Todo"}
+// 									</div>
+// 									<strong>Title:</strong>{" "}
+// 									{details.todo || "You created this Todo yourself"}
+// 									<div>
+// 										<p>
+// 											<strong>Status:</strong>{" "}
+// 											{details.completed ? "✅ Done" : "❌ Not done"}
+// 										</p>
+// 									</div>
+// 									<div className="clote-btn">
+// 										<button
+// 											onClick={handleClosePreview}
+// 											style={{
+// 												color: "white",
+// 												backgroundColor: "red",
+// 												padding: "10px 20px",
+// 												borderRadius: "10px",
+// 											}}>
+// 											<Link
+// 												style={{
+// 													color: "white",
+// 													backgroundColor: "red",
+// 													padding: "10px 20px",
+// 													borderRadius: "10px",
+// 												}}
+// 												to="/todos">
+// 												Back
+// 											</Link>
+// 										</button>
+// 									</div>
+// 								</div>
+// 							</div>
+// 						</div>
+// 					)}
+// 				</div>
+// 			</div>
+// 		</ErrorBoundary>
+// 	);
+// }
+
+import ErrorBoundary from "../components/ErrorBoundary";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { Link } from "react-router-dom";
+
+export default function SingleTodo({ details, error, loading, setDetails }) {
+	const handleClosePreview = () => {
+		setDetails(false); // Clear the preview
+	};
+
+	return (
+		<ErrorBoundary>
+			<div className="todo-item">
+				{loading && <LoadingSpinner />}
+				{error && <p className="text-red-600">Error: {error}</p>}
+				<div className="space-y-2 mb-4">
+					{details && !loading && (
+						<div>
+							<div className="preview modal-overlay">
+								<div key={details.id} className="modal-content">
+									<div>
+										<strong>Todo Details</strong>
+									</div>
+									<div>
+										<strong>ID:</strong> {details.id || "Your Todo"}
+									</div>
+									<strong>Title:</strong>{" "}
+									{details.todo || "You created this Todo yourself"}
+									<div>
+										<p>
+											<strong>Status:</strong>{" "}
+											{details.completed ? "✅ Done" : "⏳ Pending"}
+										</p>
+									</div>
+									<div className="clote-btn">
+										<button
+											onClick={handleClosePreview}
+											style={{
+												color: "white",
+												backgroundColor: "red",
+												padding: "10px 20px",
+												borderRadius: "10px",
+											}}>
+											<Link
+												style={{
+													color: "white",
+													backgroundColor: "red",
+													padding: "10px 20px",
+													borderRadius: "10px",
+												}}
+												to="/todos">
+												Back
+											</Link>
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</ErrorBoundary>
+	);
+}
